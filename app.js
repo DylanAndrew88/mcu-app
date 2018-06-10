96b2a625a7c38599f5f21fe52f632aa2bb46e7fa
DylanAndrew88 <Dandrewblake88@gmail.com>
2018-06-10 18:27:21 -0400

    initialize repo

---
+++ app.js
@@ -0,0 +1,38 @@
+
+  //Global Variables
+  const express = require('express');
+  const expressHandlebars = require('express-handlebars');
+  const moviesJSON = require('./movies.json');
+
+  const app = express();
+
+
+  //View Engine
+  app.set('view engine', 'handlebars');
+  app.engine('handlebars', expressHandlebars());
+
+
+  //Routes
+  //home
+  app.get('/', function(req, res) {
+    let movies = moviesJSON.movies;
+    res.render('home', {
+      title : "MCU App",
+      movies : movies
+    });
+  });
+  //movies
+  app.get('/movies/:title?', function(req, res) {
+    let movieTitle = req.params.title;
+    res.send("This is the page for " + movieTitle);
+  });
+  //notFound
+  app.get('*', function(req, res) {
+    res.send("<h1>This is not the page you are looking for</h1>");
+  });
+
+
+  //Port
+  app.listen(3000, function() {
+    console.log("listening on localhost:3000")
+  });
